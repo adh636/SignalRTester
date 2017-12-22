84455d7563d3092ad39bde5a32ef4c312a8cc821
Adam Hilliard <adh636@gmail.com>
2017-12-22 03:50:14 -0500

    Use owin and signal r in program

--- SignalRTester/Program.cs
+++ SignalRTester/Program.cs
@@ -1,10 +1,8 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
 using System.Reflection;
-using System.Text;
-using System.Threading.Tasks;
 using CineNet.Common.Logging;
+using Microsoft.AspNet.SignalR.Client;
+using Microsoft.Owin.Hosting;
 
 namespace SignalRTester
 {
@@ -15,6 +13,29 @@ namespace SignalRTester
             Log4NetConfiguration.SetUpLogging(Assembly.GetExecutingAssembly());
 
             Log.Error("Program Running");
+
+            string baseAddress = "http://localhost:9123/";
+
+            // Start OWIN host 
+            using (WebApp.Start<Startup>(url: baseAddress))
+            {
+                // Let's wire up a SignalR client here to easily inspect what
+                //  calls are happening
+                //
+                var hubConnection = new HubConnection(baseAddress);
+                IHubProxy eventHubProxy = hubConnection.CreateHubProxy("EventHub");
+                eventHubProxy.On<string, ChannelEvent>("OnEvent", (channel, ev) => Log.Error($"Event received on {channel} channel - {@ev}"));
+                hubConnection.Start().Wait();
+
+                // Join the channel for task updates in our console window
+                //
+                eventHubProxy.Invoke("Subscribe", Constants.AdminChannel);
+                eventHubProxy.Invoke("Subscribe", Constants.TaskChannel);
+
+                Console.WriteLine($"Server is running on {baseAddress}");
+                Console.WriteLine("Press <enter> to stop server");
+                Console.ReadLine();
+            }
         }
     }
 }
