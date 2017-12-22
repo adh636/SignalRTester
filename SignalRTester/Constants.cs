c549aba4b36d32d371714461cbb4c715c28d49e7
Adam Hilliard <adh636@gmail.com>
2017-12-22 03:51:05 -0500

    Place for constants

---
+++ SignalRTester/Constants.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SignalRTester
+{
+    public static class Constants
+    {
+        public const string AdminChannel = "admin";
+        public const string TaskChannel = "tasks";
+    }
+}
