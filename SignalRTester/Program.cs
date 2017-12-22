326e7dc95256b4f167726ac2466f5379f90693f6
Adam Hilliard <adh636@gmail.com>
2017-12-22 02:53:05 -0500

    Logging in console app

---
+++ SignalRTester/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+using CineNet.Common.Logging;
+
+namespace SignalRTester
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Log4NetConfiguration.SetUpLogging(Assembly.GetExecutingAssembly());
+
+            Log.Error("Program Running");
+        }
+    }
+}
